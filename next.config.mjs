f20a609f7fb8c1b88bcd921fc7b6c1fdb95d6cbe
Elsie Oduor <eoatieno@gmail.com>
2024-08-25 17:58:37 +0300

    Made changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: [
+        'placehold.co',
+        'example.com',
+        'another-example.com',
+        'lh3.googleusercontent.com', 
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
